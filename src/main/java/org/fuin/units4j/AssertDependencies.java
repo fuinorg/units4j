cd61482d3760c985b41fbb546e41e16b75ea2b4e
Michael Schnell <michael@fuin.org>
2024-02-10 17:42:54 +0100

    Replaced code with pre tags

--- src/main/java/org/fuin/units4j/AssertDependencies.java
+++ src/main/java/org/fuin/units4j/AssertDependencies.java
@@ -34,8 +34,8 @@ import static org.junit.jupiter.api.Assertions.fail;
  * @deprecated Use <a href="https://www.archunit.org/">ArchUnit</a> rules instead.
  *
  * Example ArchUnit test rule:
- * <code>
- * @ArchTest
+ * <pre>{@code
+ * &#64;ArchTest
  * static final ArchRule core_access_only_to_defined_packages =
  *     classes()
  *         .that()
@@ -43,7 +43,7 @@ import static org.junit.jupiter.api.Assertions.fail;
  *         .should()
  *             .onlyDependOnClassesThat()
  *             .resideInAnyPackage("my.package.common..", "java.lang..");
- * </code>
+ * }</pre>
  */
 @Deprecated
 public final class AssertDependencies {
