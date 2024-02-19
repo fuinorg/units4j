52c710f3fe6dddbeef38adbbd0f835e624fe1afb
Michael Schnell <michael@fuin.org>
2024-02-19 10:45:46 +0100

    Unarchive

--- src/test/java/org/fuin/units4j/BaseTest.java
+++ src/test/java/org/fuin/units4j/BaseTest.java
@@ -22,6 +22,7 @@ import org.fuin.units4j.analyzer.EmptyAnnotationVisitor;
 import org.fuin.units4j.analyzer.EmptyClassVisitor;
 import org.fuin.units4j.analyzer.EmptyFieldVisitor;
 import org.fuin.units4j.analyzer.EmptyMethodVisitor;
+import org.fuin.units4j.archunit.AllTopLevelClassesHaveATestCondition;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -34,8 +35,11 @@ public final class BaseTest {
         AssertCoverage.assertEveryClassHasATest(new File("src/main/java"), new ClassFilter() {
             @Override
             public boolean isIncludeClass(final Class<?> clasz) {
-                if ((clasz == EmptyClassVisitor.class) || (clasz == EmptyAnnotationVisitor.class) || (clasz == EmptyFieldVisitor.class)
-                        || (clasz == EmptyMethodVisitor.class)) {
+                if (clasz == EmptyClassVisitor.class
+                        || clasz == EmptyAnnotationVisitor.class
+                        || clasz == EmptyFieldVisitor.class
+                        || clasz == EmptyMethodVisitor.class
+                        || clasz == AllTopLevelClassesHaveATestCondition.class) {
                     return false;
                 }
                 return true;
